e1cbb4792face1f12372f0fef74799efbdf68136
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-10-06 21:01:39 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require 'pry'
+class School 
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def name 
+        @name
+    end
+
+    def roster 
+        @roster
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= []
+        roster[grade] << student
+    end
+
+    def grade(student_grade)
+        roster[student_grade]
+    end
+
+    def sort 
+        sorted = {}
+        roster.each do |grade, student|
+            sorted[grade] = student.sort
+        end
+        sorted
+    end
+
+end
